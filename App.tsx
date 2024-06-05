90ababd9a415b1a7a0500d60cfac4ef571a1413b
abhilash-1411 <Abhilash.Chandravanshi@adixoo.com>
2024-06-05 21:34:49 +0530

    Loop component using flatList

--- App.tsx
+++ App.tsx
@@ -1,118 +1,80 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+
 import {
-  SafeAreaView,
-  ScrollView,
+  FlatList,
+ ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+
+
+
+
+  
+const App=()=>{
+  const users=[
+    {
+      id:1,
+      name: 'John',
+      email:'john@gmail.com',
+    },
+    {
+      id:2,
+      name: 'Peter',
+      email:'peter@gmail.com',
+    },
+    {
+      id:3,
+      name: 'Bob',
+      email:'bob@gmail.com',
+    }
+  ]
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <Text style={{fontSize:26}}>Component in Loop with flatList</Text>
+      <FlatList
+        data={users}
+        renderItem={({item})=><UserData item={item}/>}
+      />
     </View>
   );
 }
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+const UserData=(props)=>{
+  const item=props.item;
+  return(
+    <View style={styles.box}>
+          <Text style={styles.item}>{item.name}</Text>
+          <Text style={styles.item}>{item.email}</Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+
+  )
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
+  item:{
     fontSize: 24,
-    fontWeight: '600',
+    color:'orange',
+    flex:1,
+    margin:2,
+    textAlign:'center'
+
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  box:{
+    flexDirection:'row',
+    borderWidth:2,
+    borderColor:'orange',
+    marginBottom:10,
+   
+
+  }
+ 
 });
 
 export default App;
